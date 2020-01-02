ac986ef6d3483fca26874fa5f0f0377bd2b4ed3c
Alberto Miranda <alberto.miranda@opendeusto.es>
2020-01-02 19:53:35 +0100

    django started

---
+++ ePresenceServer/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class aula(models.Model):
+    personas = models.IntegerField()
+    #Estado= 0:verde 1:amarillo 2:rojo
+    estado = models.IntegerField()
+    hora_in = models.TimeField()
+    hora_out = models.TimeField()
+def __str__(self):
+	return '%s %s %s %s' % (self.personas, self.estado, self.hora_out, self.hora_in)
